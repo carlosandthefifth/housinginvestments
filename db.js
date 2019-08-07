955442129c8bf4b95d708798c835eddb09c0ac7c
Carlos G <33984529+gcarloscodeview@users.noreply.github.com>
2019-08-07 07:01:53 -0500

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-var firstRoute  = require('./db.json');
-var secondRoute = require('./1.json');
-
-module.exports = function() {
-return {
-firstRoute  : firstRoute,
-secondRoute : secondRoute
-  }
-}
