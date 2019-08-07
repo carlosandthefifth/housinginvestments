da050287b6d3c5666a2763d25da4f9318f72c342
Carlos G <33984529+gcarloscodeview@users.noreply.github.com>
2019-08-07 06:55:11 -0500

    Create db.js

---
+++ db.js
@@ -0,0 +1,9 @@
+var firstRoute  = require('./db.json');
+var secondRoute = require('./1.json');
+
+module.exports = function() {
+return {
+firstRoute  : firstRoute,
+secondRoute : secondRoute,
+  }
+}
