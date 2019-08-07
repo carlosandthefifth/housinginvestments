a641e783d55b578784a984bfd121bbed7c3c91f7
Carlos G <33984529+gcarloscodeview@users.noreply.github.com>
2019-08-07 06:59:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
-var jsonServer  = require('json-server')
-var server      = jsonServer.create()
-var router      = jsonServer.router('./db.js')
-var middlewares = jsonServer.defaults()
+var jsonServer  = require('json-server');
 
-server.use(middlewares)
-server.use(router)
-server.listen(3000, function () {
-  console.log('JSON Server is running')
-})
+var server      = jsonServer.create();
+var router      = jsonServer.router(require('./db.js')());
+var middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(5000, function () {
+  console.log('JSON Server is running');
+});
